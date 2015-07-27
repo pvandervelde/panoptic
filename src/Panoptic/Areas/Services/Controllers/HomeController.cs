16487aaee904a8419a1cd8461da12e625f616e4c
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-07-27 23:12:31 +1200

    Adding the services area

---
+++ src/Panoptic/Areas/Services/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Panoptic">
+//     Copyright 2013 Panoptic. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Panoptic.Areas.Services.Models;
+
+namespace Panoptic.Areas.Services.Controllers
+{
+    /// <summary>
+    /// Provides the index page for the services area.
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// GET: Services/Home
+        /// </summary>
+        /// <returns>The index view.</returns>
+        public ActionResult Index()
+        {
+            var model = new ServicesModel
+            {
+                Name = "Services",
+                Description = "Providing your services.",
+            };
+            return View(model);
+        }
+    }
+}
