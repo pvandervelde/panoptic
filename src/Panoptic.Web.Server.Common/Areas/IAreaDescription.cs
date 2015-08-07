5f75afac52345a946e2d9b7eeeba70d4fe2c2dac
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-08 09:30:47 +1200

    Adding the common project

---
+++ src/Panoptic.Web.Server.Common/Areas/IAreaDescription.cs
@@ -0,0 +1,54 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Panoptic">
+//     Copyright 2013 Panoptic. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Panoptic.UI.Web.Common.Areas
+{
+    /// <summary>
+    /// Describes an area of capabilities.
+    /// </summary>
+    public interface IAreaDescription
+    {
+        /// <summary>
+        /// Gets the action that is the entry action.
+        /// </summary>
+        string Action
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the controller that provides the entry point.
+        /// </summary>
+        string Controller
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        string Description
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        string Version
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        string Name
+        {
+            get;
+        }
+    }
+}
---
+++ src/Panoptic.Web.Server.Common/Areas/IAreaDescriptionMetadata.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright company="Panoptic">
+//     Copyright 2013 Panoptic. Licensed under the Apache License, Version 2.0.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Panoptic.UI.Web.Common.Areas
+{
+    /// <summary>
+    /// Defines metadata associated with an action verb.
+    /// </summary>
+    public interface IAreaDescriptionMetadata
+    {
+        /// <summary>
+        /// Gets the category for the verb.
+        /// </summary>
+        string Category
+        {
+            get;
+        }
+    }
+}
