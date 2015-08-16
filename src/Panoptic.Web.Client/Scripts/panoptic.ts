c161b704acdee6b3d20efa8781c80e6a360cb0f6
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-17 08:37:35 +1200

    More setting up the app.

---
+++ src/Panoptic.Web.Client/Scripts/panoptic.ts
@@ -0,0 +1,19 @@
+﻿declare var angular;
+
+// Create the app and set up the routes
+var app = angular.module('panoptic', ['panoptic.filters', 'panoptic.services', 'panoptic.directives', 'panoptic.controllers'])
+    .config([<any> '$locationProvider', '$routeProvider', function ($locationProvider: any, $routeProvider: any) {
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/', {
+                controller: 'HomeController',
+                templateUrl: 'views/home.html'
+            })
+            .when('/admin', {
+                controller: 'AdminController',
+                templateUrl: 'views/admin.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
