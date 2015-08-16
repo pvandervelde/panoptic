1782b2cdaccae01e4d5a96c5ff55bc9ab7b16ae7
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-17 08:37:50 +1200

    Adding the navbar controller.

---
+++ src/Panoptic.Web.Client/Scripts/controllers/navbarcontroller.ts
@@ -0,0 +1,14 @@
+﻿declare var angular;
+
+angular.module('panoptic.controllers', [])
+    .controller('NavBarController', [<any> '$scope', function ($scope) {
+        // Store the information about the available areas here
+        $scope.Areas = [{
+            name: 'Home',
+            url: '~/home'
+        },
+        {
+            name: 'Admin',
+            url: '~/admin'
+        }]
+    }]);
