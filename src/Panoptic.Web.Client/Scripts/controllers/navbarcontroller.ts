223a0100c4c5c37234a2c5ca5d32303e00072f4b
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-22 09:46:19 +1200

    Formatting

--- src/Panoptic.Web.Client/Scripts/controllers/navbarcontroller.ts
+++ src/Panoptic.Web.Client/Scripts/controllers/navbarcontroller.ts
@@ -1,17 +1,15 @@
 ﻿///<reference path="../typings/angularjs/angular.d.ts" /> 
 ///<reference path="../typings/angularjs/angular-route.d.ts" />
 
-declare var ang;
-
-ang.module('panoptic.controllers', [])
-    .controller('NavBarController', [<any> '$scope', function ($scope) {
-        // Store the information about the available areas here
-        $scope.Areas = [{
+app.controller('NavBarController', [<any> '$scope', function ($scope) {
+    $scope.areas = [
+        {
             name: 'Home',
             url: '~/home'
         },
         {
             name: 'Admin',
             url: '~/admin'
-        }]
-    }]);
+        }
+    ];
+}]);
