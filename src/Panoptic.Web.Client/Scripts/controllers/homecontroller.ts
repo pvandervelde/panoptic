2cb9294f334877c07c84979b51f0171cfbfc3196
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-23 22:36:03 +1200

    Adding the home controller.

---
+++ src/Panoptic.Web.Client/Scripts/controllers/homecontroller.ts
@@ -0,0 +1,7 @@
+﻿///<reference path="../typings/angularjs/angular.d.ts" /> 
+///<reference path="../typings/angularjs/angular-route.d.ts" />
+
+app.controller('HomeController', [<any> '$scope', function ($scope) {
+    $scope.areaname = 'Home';
+    $scope.areadescription = 'some long text thing that just goes on for ever and ever and ever';
+}]);
