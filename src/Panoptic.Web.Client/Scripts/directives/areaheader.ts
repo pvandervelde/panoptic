31e5a0936326e620c2bd83517899674335676397
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-24 23:35:22 +1200

    Moving the directives into the scripts directory

---
+++ src/Panoptic.Web.Client/Scripts/directives/areaheader.ts
@@ -0,0 +1,9 @@
+﻿///<reference path="../typings/angularjs/angular.d.ts" /> 
+///<reference path="../typings/angularjs/angular-route.d.ts" />
+
+app.directive('areaHeader',
+    function () {
+        return {
+            templateUrl: 'Scripts/directives/areaHeader.html'
+        };
+    });
--- src/Panoptic.Web.Client/directives/areaheader.ts
+++
@@ -1,9 +0,0 @@
-﻿///<reference path="../Scripts/typings/angularjs/angular.d.ts" /> 
-///<reference path="../Scripts/typings/angularjs/angular-route.d.ts" />
-
-app.directive('areaHeader',
-    function () {
-        return {
-            templateUrl: 'directives/areaHeader.html'
-        };
-    });
