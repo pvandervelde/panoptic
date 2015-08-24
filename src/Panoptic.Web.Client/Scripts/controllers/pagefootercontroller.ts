fcc8269d2911c8ff9952bb08713fd8e246995728
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-08-24 22:16:34 +1200

    Adding the footer controller

---
+++ src/Panoptic.Web.Client/Scripts/controllers/pagefootercontroller.ts
@@ -0,0 +1,6 @@
+﻿///<reference path="../typings/angularjs/angular.d.ts" /> 
+///<reference path="../typings/angularjs/angular-route.d.ts" />
+
+app.controller('PageFooterController', [<any> '$scope', function ($scope) {
+    $scope.date = new Date();
+}]);
