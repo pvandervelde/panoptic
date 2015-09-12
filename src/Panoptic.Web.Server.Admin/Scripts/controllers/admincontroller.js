bfaba9ed2bb6f35f85cc83b4550de24b25ff0354
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-09-12 22:24:17 +1200

    Updating angular and friends

---
+++ src/Panoptic.Web.Server.Admin/Scripts/controllers/admincontroller.js
@@ -0,0 +1,7 @@
+///<reference path="../typings/angularjs/angular.d.ts" /> 
+///<reference path="../typings/angularjs/angular-route.d.ts" />
+app.controller('AdminController', ['$scope', function ($scope) {
+        $scope.areaName = 'Admin';
+        $scope.areaDescription = 'This is where you can administer your this web application from.';
+    }]);
+//# sourceMappingURL=admincontroller.js.map
---
+++ src/Panoptic.Web.Server.Admin/Scripts/controllers/admincontroller.ts
@@ -0,0 +1,8 @@
+﻿///<reference path="../typings/angularjs/angular.d.ts" /> 
+///<reference path="../typings/angularjs/angular-route.d.ts" />
+declare var app: ng.IModule;
+
+app.controller('AdminController', [<any> '$scope', function ($scope) {
+    $scope.areaName = 'Admin';
+    $scope.areaDescription = 'This is where you can administer your this web application from.';
+}]);
