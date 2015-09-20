6c382b1911939766c6848055247a230a9239c13c
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-09-20 23:25:54 +1200

    Adding the Ops project

---
+++ src/Test.Unit.Panoptic.Web.Server.Ops/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Panoptic.Web.Server.Ops;
+using Panoptic.Web.Server.Ops.Controllers;
+
+namespace Test.Unit.Panoptic.Web.Server.Ops.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        
+    }
+}
