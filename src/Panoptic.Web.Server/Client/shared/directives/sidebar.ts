166d2fe3c315f36f0ba8a4de483a34a01e3f04fd
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-11-08 20:16:57 +1300

    Adding the sidebar directive

---
+++ src/Panoptic.Web.Server/Client/shared/directives/sidebar.ts
@@ -0,0 +1,16 @@
+﻿///<reference path="../../../Scripts/typings/angularjs/angular.d.ts" /> 
+///<reference path="../../../Scripts/typings/angularjs/angular-route.d.ts" />
+
+module panoptic.shared {
+    angular.module('panoptic.shared')
+        .directive('sidebar',
+        function () {
+            return {
+                restrict: 'EA',
+                scope: {
+                    teams: "=",
+                },
+                templateUrl: 'client/shared/directives/SideBar.html'
+            };
+        });
+}
